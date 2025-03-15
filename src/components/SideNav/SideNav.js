5bc0c62b268e9e01f72a2d59b5c0506ac1814146
JiayingLei <lei1224232248@gmail.com>
2025-03-15 17:36:19 +0800

    update nav icon

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -12,7 +12,7 @@ const SideNav = ({ currentSection, onSectionSelect }) => {
   return (
     <div className="side-nav">
       <div className="nav-icon-group">
-        {/* Icon 1 */}
+        {/* Icon 1 - 拼图 */}
         <div
           className={`nav-icon-1 ${currentSection === 0 ? 'active' : ''}`}
           onClick={() => handleIconClick(0)}
@@ -23,33 +23,36 @@ const SideNav = ({ currentSection, onSectionSelect }) => {
         <div className="nav-line">
           <img src={`${process.env.PUBLIC_URL}/detailspage/icons/icon-line.png`} alt="Line" />
         </div>
-        {/* Icon 2 */}
+
+        {/* Icon 2 - 实际图像 */}
         <div
           className={`nav-icon-2 ${currentSection === 1 ? 'active' : ''}`}
           onClick={() => handleIconClick(1)}
-          data-tooltip={"保护级别\n Protection Level"}
+          data-tooltip={"实际图像\n Actual Image"}
         >
           <img src={`${process.env.PUBLIC_URL}/detailspage/icons/icon-2.png`} alt="Icon 2" />
         </div>
         <div className="nav-line">
           <img src={`${process.env.PUBLIC_URL}/detailspage/icons/icon-line.png`} alt="Line" />
         </div>
-        {/* Icon 3 (Shared by section 3 and 3A) */}
+
+        {/* Icon 3 - 濒危原因 */}
         <div
           className={`nav-icon-3 ${currentSection === 2 ? 'active' : ''}`}
           onClick={() => handleIconClick(2)} 
           data-tooltip={"濒危原因\n Endangerment Reasons"}
         >
-          <img src={`${process.env.PUBLIC_URL}/detailspage/icons/icon-3.png`} alt="Icon 3 & 3A" />
+          <img src={`${process.env.PUBLIC_URL}/detailspage/icons/icon-3.png`} alt="Icon 3" />
         </div>
         <div className="nav-line">
           <img src={`${process.env.PUBLIC_URL}/detailspage/icons/icon-line.png`} alt="Line" />
         </div>
-        {/* Icon 4 */}
+
+        {/* Icon 4 - 保护级别 */}
         <div
           className={`nav-icon-4 ${currentSection === 3 ? 'active' : ''}`}
           onClick={() => handleIconClick(3)}
-          data-tooltip={"实际图像\n Actual Image"}
+          data-tooltip={"保护级别\n Protection Level"}
         >
           <img src={`${process.env.PUBLIC_URL}/detailspage/icons/icon-4.png`} alt="Icon 4" />
         </div>
